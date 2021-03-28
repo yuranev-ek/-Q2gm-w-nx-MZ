98073d13c4fb2d0f0c4db9b5aa172c7de0ff18f0
Evgeny Yuranev <yuranev.ek@gmail.com>
2021-03-28 20:10:33 +0300

    feat(table): basic table component

---
+++ src/components/BaseTable.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <table class="table-fixed w-full">
+      <thead>
+        <tr>
+          <template v-for="(col, colIndex) of columns">
+            <th :key="`col-${colIndex}`" v-if="col.visible">
+              {{ col.label }}
+            </th>
+          </template>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(row, rowIndex) of rows" :key="`row-${rowIndex}`">
+          <template v-for="(td, tdIndex) of columns">
+            <td :key="`td-${tdIndex}`" v-if="td.visible">
+              {{ row[td.key] }}
+            </td>
+          </template>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseTable",
+  props: {
+    columns: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+    rows: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  computed: {
+    visibleColumns() {
+      return this.columns.filter((col) => col.visible).map((col) => col.key);
+    },
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,17 @@
 <template>
-  <div></div>
+  <div class="container mx-auto px-4">
+    <base-table :rows="smallData" :columns="clientTableColumns" />
+  </div>
 </template>
 
 <script>
 import { getSmallData, getBigData } from "@/api";
+import BaseTable from "@/components/BaseTable.vue";
 export default {
   name: "Home",
+  components: {
+    BaseTable,
+  },
   mounted() {
     this.getSmallData();
     this.getBigData();
@@ -14,6 +20,43 @@ export default {
     return {
       smallData: [],
       bigData: [],
+      clientTableColumns: [
+        {
+          key: "id",
+          label: "ID",
+          visible: true,
+        },
+        {
+          key: "firstName",
+          label: "First Name",
+          visible: true,
+        },
+        {
+          key: "lastName",
+          label: "Last Name",
+          visible: true,
+        },
+        {
+          key: "email",
+          label: "Email",
+          visible: true,
+        },
+        {
+          key: "phone",
+          label: "Phone",
+          visible: true,
+        },
+        {
+          key: "address",
+          label: "Address",
+          visible: false,
+        },
+        {
+          key: "description",
+          label: "Description",
+          visible: false,
+        },
+      ],
     };
   },
   methods: {
