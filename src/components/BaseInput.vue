a66b8c952d9953ea0594d4e00d1c47ee60e5d872
Evgeny Yuranev <yuranev.ek@it-rkomi.ru>
2021-03-29 13:35:29 +0300

    feat(input): basic input component

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="">
+    <label class="">
+      <div class="">{{ label }}</div>
+      <input
+        :value="value"
+        :type="type"
+        class="w-full pl-3 pr-10 py-2 border-2 border-gray-200 rounded-xl hover:border-gray-300 focus:outline-none focus:border-blue-500 transition-colors"
+        :placeholder="placeholder"
+        @input="onInput"
+      />
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseInput",
+  props: {
+    value: {
+      type: [String, Number],
+      default: null,
+    },
+    type: {
+      type: String,
+      default: "text",
+    },
+    label: {
+      type: String,
+      default: null,
+    },
+    placeholder: {
+      type: String,
+      default: null,
+    },
+  },
+  model: {
+    prop: "value",
+    event: "on-input",
+  },
+  methods: {
+    onInput(event) {
+      this.$emit("on-input", event.target.value);
+    },
+  },
+};
+</script>
